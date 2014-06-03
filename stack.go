a9fd6fbcfc2fe03898c115e95b89af525339d5f0
Trevor Killeen <killeent@cs.washington.edu>
2014-06-03 14:31:46 -0700

    add godoc support for stack and package; fix unintended broken encapsulation

--- stack.go
+++ stack.go
@@ -1,22 +1,29 @@
+/*
+Package goalgo implements standard CS data structures and algorithms.
+*/
 package goalgo
 
 import "errors"
 
+// Stack is a FIFO data structure.
 type Stack struct {
-	top  *Element
+	top  *element
 	size int
 }
 
-type Element struct {
-	next *Element
+type element struct {
+	next *element
 	data int
 }
 
+// Push adds an element to the top of the Stack.
 func (s *Stack) Push(data int) {
-	s.top = &Element{next: s.top, data: data}
+	s.top = &element{next: s.top, data: data}
 	s.size++
 }
 
+// Pop removes and returns the top element in the Stack. If
+// the Stack is empty, returns a non-nil error.
 func (s *Stack) Pop() (int, error) {
 	if s.size > 0 {
 		data := s.top.data
@@ -27,6 +34,7 @@ func (s *Stack) Pop() (int, error) {
 	return 0, errors.New("Stack is empty")
 }
 
+// Count returns the number of elements in the Stack.
 func (s *Stack) Count() int {
 	return s.size
 }
