231cc7512653e59ae03251f627c6f7705dfa6c18
Trevor Killeen <killeent@cs.washington.edu>
2014-06-03 16:04:59 -0700

    finish stack_test

--- stack_test.go
+++ stack_test.go
@@ -8,17 +8,59 @@ import (
 func TestSingleAddRemove(t *testing.T) {
 	st := new(Stack)
 	st.Push(0)
-	if st.Count() != 1 {
-		t.Errorf("Expected count: %d, Actual: %d", 1, st.Count())
+	assertEqual(t, 1, st.Count())
+	assertEqual(t, 0, extractInt(t, st.Pop()))
+	assertEqual(t, 0, st.Count())
+}
+
+// TestMultipleAddRemove tests adding and removing a series of ints to the
+// Stack.
+func TestMultipleAddRemove(t *testing.T) {
+	st := new(Stack)
+	st.Push(1)
+	st.Push(2)
+	st.Push(3)
+	assertEqual(t, 3, st.Count())
+	assertEqual(t, 3, extractInt(t, st.Pop()))
+	assertEqual(t, 2, st.Count())
+	assertEqual(t, 2, extractInt(t, st.Pop()))
+	assertEqual(t, 1, st.Count())
+	assertEqual(t, 1, extractInt(t, st.Pop()))
+	assertEqual(t, 0, st.Count())
+}
+
+// TestInterleavedPushPop tests interleaved addition and removal of elements
+// to the Stack.
+func TestInterleavedPushPop(t *testing.T) {
+	st := new(Stack)
+	st.Push(1)
+	st.Push(2)
+	assertEqual(t, 2, extractInt(t, st.Pop()))
+	st.Push(3)
+	assertEqual(t, 3, extractInt(t, st.Pop()))
+	assertEqual(t, 1, extractInt(t, st.Pop()))
+	assertEqual(t, 0, st.Count())
+}
+
+// TestEmptyStackNiReturn tests that trying to remove an element from an empty
+// Stack returns nil.
+func TestEmptyStackNilReturn(t *testing.T) {
+	st := new(Stack)
+	if st.Pop() != nil {
+		t.Error("Invalid return type")
 	}
-	data, ok := st.Pop().(int)
+}
+
+func extractInt(t *testing.T, data interface{}) int {
+	value, ok := data.(int)
 	if !ok {
-		t.Errorf("Invalid return type")
+		t.Error("Invalid return type")
 	}
-	if data != 0 {
-		t.Errorf("Expected value: %d, Actual: %d", 0, data)
-	}
-	if st.Count() != 0 {
-		t.Errorf("Expected count: %d, Actual: %d", 0, st.Count())
+	return value
+}
+
+func assertEqual(t *testing.T, expected, actual int) {
+	if actual != expected {
+		t.Errorf("Expected: %d, Actual: %d", expected, actual)
 	}
 }
