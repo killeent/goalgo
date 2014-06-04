d44a0c62ba50c54ee2e8e7a2ea47c9f34327d8fd
Trevor Killeen <killeent@cs.washington.edu>
2014-06-04 15:24:42 -0700

    remove need for swap function in insertion sort

--- sort.go
+++ sort.go
@@ -4,14 +4,7 @@ func insertionSort(slice []int) {
 	for i := 1; i < len(slice); i++ {
 		j := i
 		for j > 0 && slice[j-1] > slice[j] {
-			swap(slice, j-1, j)
-			j--
+			slice[j-1], slice[j] = slice[j], slice[j-1]
 		}
 	}
-}
-
-func swap(slice []int, i, j int) {
-	temp := slice[i]
-	slice[i] = slice[j]
-	slice[j] = temp
 }
